ccd1d564625d552731f2283165f0bbba46acc807
deedas04 <130046150+deedas04@users.noreply.github.com>
2023-04-27 11:14:58 +0530

    2 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-// carbon-components-angular default imports
-import { UIShellModule, IconModule } from 'carbon-components-angular';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, IconModule],
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
